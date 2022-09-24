c86af5265cf2b691aa735ffd9315b1b6a0867533
DANIELSSF <daniel.santiago730@gmail.com>
2022-09-23 21:49:56 -0500

    The middlewares import and moongose check have been added

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,38 @@
 */
 
 const { Router } = require('express');
-const route = Router();
+const { check } = require('express-validator');
 
+const { validateFields } = require('../middlewares/validateFields');
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
+const route = Router();
+
+
 //* POST
-route.post('/new', createUser);
-route.post('/', loginUser);
+route.post(
+    '/new',
+    [   //* Middelwares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password es obligatorio').isLength({ min: 6 }),
+        //! Middelwares
+        validateFields,
+    ],
+    createUser
+);
+
+
+route.post(
+    '/',
+    [ //* Middelware
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria').isLength({ min: 6 }),
+        //! Middelwares
+        validateFields,
+    ],
+    loginUser
+);
 
 //*GET
 route.get('/renew', renewToken);
