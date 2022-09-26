39f597b04c57042327d3391d0a052ab2913d3a21
DANIELSSF <daniel.santiago730@gmail.com>
2022-09-26 06:00:30 -0500

    Events Routes has been created

---
+++ routes/events.js
@@ -0,0 +1,37 @@
+/*
+    Rutas para events // Events Routes
+    host+/api/events
+*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validateFields');
+
+const { createEvent, getEvents, updateEvent, deleteEvent } = require('../controllers/events');
+const { validateJWT } = require('../middlewares/validateJWT');
+const { isDate } = require('../helpers/isDate');
+
+const route = Router();
+
+//* Todos tienen que pasar por la validación del token
+route.use(validateJWT);
+
+//* GET
+route.get('/', getEvents);
+
+//* POST
+route.post(
+    '/',
+    [
+        check('title', 'El titulo es obliagtorio').not().isEmpty(),
+        check('start', 'El evento debe tener una fecha de inicio').custom(isDate),
+        check('end', 'El evento debe tener una fecha de finalización').custom(isDate),
+        validateFields
+    ],
+    createEvent);
+route.post('/:id', updateEvent);
+
+//* DELETE
+route.delete('/:id', deleteEvent);
+
+
+module.exports = route;
